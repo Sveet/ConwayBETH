e5f3b5734d7fe93a0dbdd51d6f0863ebe63e0749
Joseph Fortman <jdbf89@gmail.com>
2023-09-03 14:53:29 -0500

    basic grid rendering (WIP)

--- src/index.tsx
+++ src/index.tsx
@@ -12,24 +12,33 @@ const BaseHtml = ({ children }: elements.Children) => `
   <script src="https://unpkg.com/htmx.org@1.9.5"></script>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
-
+<body>
 ${children}
-
+</body>
 </html>
 `
+const grid = Array(12).map(g => new Array(12));
 const app = new Elysia()
   .use(html())
-  .get("/", ({html}) => 
+  .get("/", ({html}) =>
   html(
     <BaseHtml>
-      <body>
-        <button hx-post="/clicked" hx-swap="outerHTML">
-          Click Me
-        </button>
-      </body>
+      <button hx-post="/clicked" hx-swap="outerHTML">
+        Click Me
+      </button>
+      ${renderGrid(grid)}
     </BaseHtml>
   )
   )
   .post("/clicked", () => <div>I'm from the server!</div>)
   .listen(3000);
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+
+const Cell = (alive: boolean) => alive ? <div class="bg-black">x</div> : <div class="bg-white">x</div>
+
+const renderGrid = (grid: boolean[][]) => {
+  const cells = grid.map(r => r.map(c => Cell(c)))
+  return <div class="grid grid-cols-12 aspect-square mx-auto">
+    ${cells}
+  </div>
+}
